2385c3d852b37cc59f1e8bb340803a3ed3db6f0a
tjackson61 <tylerj_ar@yahoo.com>
2023-12-12 17:53:15 -0500

    Top N input added to make chart function.

--- app.R
+++ app.R
@@ -3,14 +3,26 @@ library(plotly)
 library(readr)
 library(pool)
 
+SORT_TYPE_CHOICES <- c("None", "Desc", "Asc")
 
 ui <- function() {
   shiny::fluidPage(
 
     # Input Panel -------------------------------------------------------------
     shiny::inputPanel(
-      shiny::numericInput("topn", "Top N", value = 10, min = 5, max = 15, step = 1),
-      shiny::selectInput("sort_type", "Sort Type", choices = c("None", "Desc", "Asc")),
+      shiny::numericInput(
+        "topn",
+        "Top N",
+        value = 10,
+        min = 5,
+        max = 15,
+        step = 1
+      ),
+      shiny::selectInput(
+        "sort_type",
+        "Sort Type",
+        choices = SORT_TYPE_CHOICES
+      ),
       shiny::actionButton("submit", "Submit")
     ),
     shiny::fluidRow(
@@ -24,10 +36,18 @@ ui <- function() {
 
 server <- function(input, output, session) {
   source("chart.R")
+  source("utils/exec_query.R")
+  source("utils/get_query.R")
+
+  flight_data <- exec_query(sql_query)
 
 
+  # Event Listeners ---------------------------------------------------------
+
+
+  # Output ------------------------------------------------------------------
   output$chart <- plotly::renderPlotly({
-    make_chart(sort_data = TRUE, top_n = 10)
+    make_chart(flight_data, sort_data = TRUE, top_n = input$topn)
   })
 }
 
--- chart.R
+++ chart.R
@@ -1,20 +1,14 @@
-make_chart <- function(type = "bar", sort_data = TRUE, top_n = 10) {
-  source("utils/get_csv_data.R") # comment out once query functionality working
-  source("utils/exec_query.R")
-  source("utils/get_query.R")
-
-  flight_data <- exec_query(sql_query)
-
-
+make_chart <- function(data , type = "bar", sort_data = TRUE, top_n = 10) {
+  
   # parameter control
   if (sort_data) {
-    flight_data <- flight_data |>
+    flight_data <- data |>
       # sort functionality
       dplyr::arrange(desc(AVERAGE_TOTAL_DURATION)) |>
       # top N functionality
       dplyr::slice_head(n = top_n)
   }
-  exec_query
+  
   result <- flight_data |>
     plotly::plot_ly() |>
     plotly::add_trace(x = ~CRAFT_MODEL_NAME, y = ~AVERAGE_TOTAL_DURATION, type = type)
