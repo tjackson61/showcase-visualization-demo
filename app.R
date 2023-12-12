3dc3f91665269cb2a63eb147b26cc9339e00fd2f
tjackson61 <tylerj_ar@yahoo.com>
2023-12-12 16:01:24 -0500

    Added shiny-run for background updates, and Top N / Sort Type options inputs

--- app.R
+++ app.R
@@ -5,15 +5,23 @@ library(pool)
 
 
 ui <- function() {
-  shiny::basicPage(
-    plotly::plotlyOutput('chart'),
-    shiny::actionButton("submit", "Submit")
+  shiny::fluidPage(
+    shiny::inputPanel(
+      shiny::numericInput("topn", "Top N", value = 10, min = 5, max = 15, step = 1),
+      shiny::selectInput("sort_type", "Sort Type", choices = c("None", "Desc", "Asc")),
+    ),
+    shiny::inputPanel(
+      shiny::actionButton("submit", "Submit")
+    ),
+    shiny::fluidRow(plotly::plotlyOutput("chart"))
   )
 }
 
 
 server <- function(input, output, session) {
   source("chart.R")
+
+
   output$chart <- plotly::renderPlotly({
     make_chart(sort_data = TRUE, top_n = 10)
   })
---
+++ run_shiny.R
@@ -0,0 +1,2 @@
+options(shiny.autoreload = TRUE)
+shiny::runApp()
