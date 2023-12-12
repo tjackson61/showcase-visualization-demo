2c85b831db576e98f03f5e8a8856d5bb7058ff4b
tjackson61 <tylerj_ar@yahoo.com>
2023-12-12 18:40:03 -0500

    In progress to make X axis category filter and Measure Type Filter for drill accross.

--- app.R
+++ app.R
@@ -2,6 +2,7 @@ library(shiny)
 library(plotly)
 library(readr)
 library(pool)
+library(forcats)
 
 SORT_TYPE_CHOICES <- c("None", "Desc", "Asc")
 
@@ -23,7 +24,18 @@ ui <- function() {
         "Sort Type",
         choices = SORT_TYPE_CHOICES
       ),
-      shiny::actionButton("submit", "Submit")
+      shiny::varSelectInput(
+        "xaxis",
+        "X Axis Category",
+        data = exec_query(sql_query)
+        # make different query to call only dimensions
+      ),
+      shiny::selectizeInput(
+        "value_filter",
+        "Y Values Type",
+        choices = ""
+        # make different query to call only measures
+      )
     ),
     shiny::fluidRow(
       shiny::wellPanel(
@@ -39,15 +51,44 @@ server <- function(input, output, session) {
   source("utils/exec_query.R")
   source("utils/get_query.R")
 
-  flight_data <- exec_query(sql_query)
+  flight_data <- reactive(exec_query(sql_query))
 
 
   # Event Listeners ---------------------------------------------------------
+  observeEvent(input$xaxis,
+    {
+      shiny::updateVarSelectInput(
+        session,
+        "xaxis",
+        data = flight_data()
+      )
+    },
+    once = TRUE
+  )
+
+  observeEvent(input$xaxis, {
+    # 
+    select_choices <- flight_data() |>
+      select(input$xaxis) |>
+      unique()
+    
+    # Update using the reactive unique selected column
+    shiny::updateSelectizeInput(
+      session,
+      "value_filter",
+      choices = select_choices
+    )
+  })
+
 
 
   # Output ------------------------------------------------------------------
   output$chart <- plotly::renderPlotly({
-    make_chart(flight_data, sort_data = TRUE, top_n = input$topn)
+    # data <- flight_data() |>
+    #   filter(CRAFT_MODEL_NAME == input$value_filter) |>
+    #   select(input$value_filter)
+    data <- flight_data()
+    make_chart(data, sort_data = TRUE, top_n = input$topn)
   })
 }
 
--- chart.R
+++ chart.R
@@ -1,17 +1,20 @@
-make_chart <- function(data , type = "bar", sort_data = TRUE, top_n = 10) {
-  
-  # parameter control
-  if (sort_data) {
-    flight_data <- data |>
-      # sort functionality
-      dplyr::arrange(desc(AVERAGE_TOTAL_DURATION)) |>
-      # top N functionality
-      dplyr::slice_head(n = top_n)
-  }
-  
-  result <- flight_data |>
+make_chart <- function(data, type = "bar", sort_data = TRUE, top_n = 10) {
+  result <- data |>
+    mutate(forcats::fct(CRAFT_MODEL_NAME)) |>
+    arrange(desc(AVERAGE_TOTAL_DURATION)) |>
+    dplyr::slice_head(n = top_n) |>
     plotly::plot_ly() |>
-    plotly::add_trace(x = ~CRAFT_MODEL_NAME, y = ~AVERAGE_TOTAL_DURATION, type = type)
+    plotly::add_trace(
+      x = ~CRAFT_MODEL_NAME,
+      y = ~AVERAGE_TOTAL_DURATION,
+      type = type
+    ) |>
+    layout(
+      xaxis = list(
+        categoryorder = "array",
+        categoryarray = ~ reorder(CRAFT_MODEL_NAME, AVERAGE_TOTAL_DURATION)
+      )
+    )
 
   return(result)
 }
